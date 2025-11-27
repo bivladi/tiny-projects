61f76431fae1e4ac5bafbbda977435c8da4bbc98
vladislav.bukharov <vladislav.bukharov@presight.ai>
2025-11-27 13:27:29 +0400

    logger start

---
+++ logger/go.mod
@@ -0,0 +1,3 @@
+module learngo/logger
+
+go 1.25.0
---
+++ logger/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learngo/logger/pocketlog"
+)
+
+func main() {
+	var lvl pocketlog.Logger
+	fmt.Printf("Level: %v\n", lvl)
+}
